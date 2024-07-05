84e7a7c288bb421b9d0b8b7db790ecf32c78fafc
Al Ayyubi Sidik <eka03juli1980@gmail.com>
2024-07-05 07:47:20 +0700

    feat: add router

---
+++ app/router.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"gallery_go/controller"
+	"gallery_go/middleware"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func NewRouter(userController controller.UserController) *httprouter.Router {
+	router := httprouter.New()
+
+	router.POST("/api/v1/users/signup", userController.SignUp)
+	router.POST("/api/v1/users/signin", userController.SignIn)
+	router.DELETE("/api/v1/users/signout", middleware.ChainMiddleware(userController.SignOut, middleware.AuthMiddleware))
+	router.GET("/api/v1/users/currentuser", middleware.ChainMiddleware(userController.CurrentUser, middleware.AuthMiddleware))
+	router.PUT("/api/v1/users/:userId", middleware.ChainMiddleware(userController.Update, middleware.AuthMiddleware))
+
+	return router
+}
