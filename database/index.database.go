32e2fa6369283557e0333059f777df197fd84cc6
Al Ayyubi Sidik <eka03juli1980@gmail.com>
2024-07-07 22:00:12 +0700

    feat: add migration files for users table

--- database/index.database.go
+++ database/index.database.go
@@ -25,3 +25,10 @@ func ConnectDatabase() {
 
 	log.Println("Connected to database")
 }
+
+// migrate create -ext sql -dir database/migrations create_users_table
+
+// migrate -database "mysql://root:@tcp(127.0.0.1:3306)/gallery_go?charset=utf8mb4&parseTime=True&loc=Local" -path database/migrations up
+// migrate -database "mysql://root:@tcp(127.0.0.1:3306)/gallery_go?charset=utf8mb4&parseTime=True&loc=Local" -path database/migrations down
+// migrate -database "mysql://root:@tcp(127.0.0.1:3306)/gallery_go?charset=utf8mb4&parseTime=True&loc=Local" -path database/migrations force 9863498326134
+// migrate -database "mysql://root:@tcp(127.0.0.1:3306)/gallery_go?charset=utf8mb4&parseTime=True&loc=Local" -path database/migrations version
