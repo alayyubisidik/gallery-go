b273604793b8097aeaf314ac76f2b99e83a8efb0
Al Ayyubi Sidik <eka03juli1980@gmail.com>
2024-07-08 15:20:33 +0700

    feat: add controller and testing

--- test/test_utils.go
+++ test/test_utils.go
@@ -1,11 +1,14 @@
 package test
 
 import (
-	"gorm.io/gorm"
 	controller "gallery_go/controllers"
+	"gallery_go/database"
 	"gallery_go/exception"
-	"github.com/gin-gonic/gin"
+	"gallery_go/helper"
+	"gallery_go/models"
 
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func InitRouteTest(app *gin.Engine) *gin.Engine {
@@ -13,6 +16,7 @@ func InitRouteTest(app *gin.Engine) *gin.Engine {
 
 	route.Use(exception.GlobalErrorHandler())
 	route.POST("/api/v1/users/signup", controller.SignUp)
+	route.POST("/api/v1/users/signin", controller.SignIn)
 
 	return route
 }	
@@ -20,4 +24,21 @@ func InitRouteTest(app *gin.Engine) *gin.Engine {
 func DeleteTestUsernames(db *gorm.DB) {
     // Hapus semua username yang berawalan "test"
     db.Exec("DELETE FROM users WHERE username LIKE 'test%'")
+}
+
+func CreateUser(username string, email string) models.User {
+	hashedPassword, err := helper.HashPassword("test")
+	helper.PanicIfError(err)
+
+	user := models.User{
+		Username: username,
+		FullName: "Test",
+		Email: email,
+		Password: hashedPassword,
+	}
+
+	err = database.DB.Save(&user).Error
+	helper.PanicIfError(err)
+	
+	return user
 }
--- test/user_controller_test.go
+++ test/user_controller_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"encoding/json"
+	"fmt"
 	"gallery_go/database"
 	"gallery_go/helper"
 	"io"
@@ -15,7 +16,6 @@ import (
 	"github.com/joho/godotenv"
 
 	"github.com/stretchr/testify/assert"
-
 )
 
 func TestSignUpSuccess(t *testing.T) {
@@ -81,4 +81,74 @@ func TestSignUpFailed(t *testing.T) {
 
 	response := recorder.Result()
 	assert.Equal(t, 400, response.StatusCode)
+}
+
+func TestSignInSuccess(t *testing.T) {
+	envPath, err := filepath.Abs("../.env")
+	helper.PanicIfError(err)
+	err = godotenv.Load(envPath)
+	helper.PanicIfError(err)
+
+	database.ConnectDatabase()
+	app := gin.Default()
+	DeleteTestUsernames(database.DB)
+	CreateUser("test", "test@gmail.com")
+	router := InitRouteTest(app)
+
+
+	requestBody := strings.NewReader(`{
+		"username": "test",
+		"password": "test"
+	}`)
+
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:3000/api/v1/users/signin", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 200, response.StatusCode)
+	
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]any
+	json.Unmarshal(body, &responseBody)
+
+	assert.Equal(t, "test", responseBody["data"].(map[string]any)["username"])
+	assert.Equal(t, "Test", responseBody["data"].(map[string]any)["full_name"])
+	assert.Equal(t, "test@gmail.com", responseBody["data"].(map[string]any)["email"])
+	assert.Equal(t, "author", responseBody["data"].(map[string]any)["role"])
+}
+
+func TestSignInFailed(t *testing.T) {
+	envPath, err := filepath.Abs("../.env")
+	helper.PanicIfError(err)
+	err = godotenv.Load(envPath)
+	helper.PanicIfError(err)
+
+	database.ConnectDatabase()
+	app := gin.Default()
+	DeleteTestUsernames(database.DB)
+	CreateUser("tests", "test@gmail.com")
+	router := InitRouteTest(app)
+
+	requestBody := strings.NewReader(`{
+		"username": "test",
+		"password": "test"
+	}`)
+
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:3000/api/v1/users/signin", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 401, response.StatusCode)
+	
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]any
+	json.Unmarshal(body, &responseBody)
+
+	fmt.Println(responseBody)
 }
